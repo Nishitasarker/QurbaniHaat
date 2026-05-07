0503d91512bc0f975bd1f0e3fd7e3eb6d84d6b83
Nishitasarker <shuvrodevsaikat71@gmail.com>
2026-05-07 07:09:10 +0600

    Extra sections: Qurbani Tips and Top Breeds added here

--- src/app/ExtraSections/page.jsx
+++ src/app/ExtraSections/page.jsx
@@ -27,7 +27,7 @@ const ExtraSection = () => {
     <div>
      <h2 className="text-3xl font-bold text-center mb-10 text-success">Top Recommended Breeds</h2>
         
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
           {breeds.map((animal) => (
             <div key={animal.id} className="card bg-white shadow-xl overflow-hidden border-b-4 border-lime-500">
               <figure className="relative w-full aspect-square">
@@ -46,9 +46,9 @@ const ExtraSection = () => {
                   {animal.id === 2 && "Domestic Pride"}
                 </p>
                 <p className="text-gray-600 text-sm mt-2">{animal.description.slice(0, 80)}...</p>
-                <div className="card-actions justify-start mt-4">
-                  <div className="badge badge-outline">Weight: {animal.weight}kg</div>
-                  <div className="badge badge-outline">Age: {animal.age}yrs</div>
+                <div className="card-actions justify-start ">
+                  <div className="badge badge-outline p-2">Weight: {animal.weight}kg</div>
+                  <div className="badge badge-outline p-2">Age: {animal.age}yrs</div>
                 </div>
               </div>
             </div>
@@ -60,7 +60,7 @@ const ExtraSection = () => {
 {/* right-side */}
       <div>
 <h2 className="text-3xl font-bold text-center mb-10 text-success">Essential  Qurbani Tips</h2>
-<div className='grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-11 py-1 '>
+<div className='grid grid-cols-1 md:grid-cols-2 gap-3  py-5 '>
 
  <div className="card card-side bg-base-100 shadow-sm">
 <div className='flex items-center justify-center'><MdOutlineBalance className='h-15 w-15 bg-lime-200 rounded-full p-2
